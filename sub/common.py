2407a4ea3feda27d3670ad178080e165ace9d0f0
Yutaro NIRASAWA <137530323+yuu-nirasawa@users.noreply.github.com>
2025-09-30 22:27:26 +0900

    Update common.py

--- sub/common.py
+++ sub/common.py
@@ -1,36 +1,3 @@
-####################
-# Useful programs
-#
-# How to use:
-#   COCO(ddir,coco_type='COCO025')
-#       ddir        : directory of COCO grid data
-#       coco_type   : resolution type of COCO ('COCO100', 'COCO025', 'COCO010')
-#
-#   GLORYS12v1(ddir,ymdh)
-#       ddir    : directory of GLORYS12v1 data
-#       ymdh    : yyyymmddHH (anything is ok)
-#
-#   LUT(coco,glorys)
-#       coco    : COCO grid information (object variable)
-#       glorys  : GLORYS12v1 grid information (object variable)
-#
-#   HEADER(ddir,ymdh)
-#       .write(tfout,num)
-#       ddir    : directory of COCO restart file
-#       ymdh    : yyyymmddHH (anything is ok)
-#       tfout   : output file
-#       num     : variable number
-#
-#   CHECK(ifname,ofname,varname,grid1,grid2,tlev)
-#       ifname  : file name of original data
-#       ofname  : file name of created data
-#       varname : target variable name ('to','so')
-#       grid1   : grid of original data (object variable)
-#       grid2   : grid of created data (object variable)
-#       tlev    : target level [m]
-#
-####################
-
 import os
 import sys
 import numpy as np
